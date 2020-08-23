99faf410dfd1df5406aaee3c712ec4d90ff7b9d3
Gaurav <gusaingaurav12345@gmail.com>
2020-08-23 15:47:38 +0530

    added register errors in the admin form

--- resources/views/admin.blade.php
+++ resources/views/admin.blade.php
@@ -9,7 +9,7 @@
         <br>
         <button class="btn btn-outline-primary" id="admin-login-btn">Login</button>
         <button class="btn btn-outline-dark" id="admin-register-btn">Register</button>
-        @if($errors->any())
+        @if($errors->any())     <!--error condition method 1-->
         <h6 style="color:red;">There are errors in the form.</h6>
         @endif
     </div>
@@ -41,26 +41,34 @@
         @csrf
             <div class="form-group">
                 <label>Name: </label>
-                <input class="form-control shadow" type="text" name="name">
+                <input class="form-control shadow @error('name') border-danger @enderror" type="text" name="name">
+                @error('name')
                 <p style="color:red;">{{$errors->first('name')}}</p>
+                @enderror
             </div>
             <div class="form-group">
                 <label>Email: </label>
                 <div class="input-group">
-                    <input class="form-control shadow" type="text" name="email">
+                    <input class="form-control shadow @error('email') border-danger @enderror" type="text" name="email">
                     <span class="input-group-append input-group-text">@company.com</span>
                 </div>
+                @error('name')
                 <p style="color:red;">{{$errors->first('email')}}</p>
+                @enderror
             </div>
             <div class="form-group">
                 <label>Password: </label>{{$errors->first('password')}}
-                <input class="form-control shadow" type="password" name="password1">
+                <input class="form-control shadow @error('password1') border-danger @enderror" type="password" name="password1">
+                @if($errors->has('password1'))
                 <p style="color:red;">{{$errors->first('password1')}}</p>
+                @endif
             </div>
             <div class="form-group">
                 <label>Retype Password: </label>
-                <input class="form-control shadow" type="password" name="password2">
+                <input class="form-control shadow @error('password2') border-danger @enderror" type="password" name="password2">
+                @if($errors->has('password2'))
                 <p style="color:red;">{{$errors->first('password2')}}</p>
+                @endif
             </div>
             <input class="btn btn-success shadow" type="submit" value="Register"> 
         </form>
